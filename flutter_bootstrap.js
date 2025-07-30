3e40fd8fccf9e95afa1cab6ab2f179d06b7d64d9
TheBestSoftwareEngineer <MOHAMEDHAMDY192001@gmail.com>
2025-07-31 01:27:43 +0300

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3038853557"
+    serviceWorkerVersion: "4018320932"
   }
 });
